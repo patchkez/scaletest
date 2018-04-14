f39741c14d6f80fd7b99f5a2da51dd1b831cc10e
blackjok3r <blackjok3r@gmail.com>
2018-04-15 01:57:56 +0800

    modified:   blastloop.sh 	modified:   txsend

--- blastloop.sh
+++ blastloop.sh
@@ -1,7 +1,13 @@
 #!/bin/bash 
+if [[ $1 = "" ]]
+then
+ echo "no transaction size specified"
+ exit
+fi
+
 COUNTER=0
 while [  $COUNTER -lt 3 ]; do
-  ./txsend $1 &
-  sleep 7
+  ./txblast $1 &
+  sleep 10
   let COUNTER=COUNTER+1
 done
