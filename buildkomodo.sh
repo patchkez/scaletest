0d01898314d160de73227e7e864a67eac5d2890c
blackjok3rtt <30971146+blackjok3rtt@users.noreply.github.com>
2018-04-16 19:01:08 +0800

    mergemaster

--- buildkomodo.sh
+++ buildkomodo.sh
@@ -1,10 +1,11 @@
 #Install Deps
+sudo apt-get update
 sudo apt-get install build-essential pkg-config libc6-dev m4 g++-multilib autoconf libtool ncurses-dev unzip git python python-zmq zlib1g-dev wget libcurl3-gnutls-dev bsdmainutils automake curl
 #Install Komodo
 cd ~
 git clone https://github.com/jl777/komodo
 cd komodo
-git checkout jl777
+git checkout mergemaster
 ./zcutil/fetch-params.sh
 ./zcutil/build.sh -j$(nproc)
 sudo ln -sf /home/$USER/komodo/src/komodo-cli /usr/local/bin/komodo-cli
