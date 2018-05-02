92642c4e6536f79d31267a6aa2ef917acf1a1ac1
blackjok3r <blackjok3r@gmail.com>
2018-05-02 18:46:04 +0200

    modified:   m_notary_scale

--- applytweaks.sh
+++ applytweaks.sh
@@ -2,5 +2,9 @@
 sudo cp 01-notary.conf /etc/sysctl.d/
 echo "$USER soft nofile 1000000" | sudo tee --append /etc/security/limits.conf
 echo "$USER hard nofile 1000000" | sudo tee --append /etc/security/limits.conf
+echo "hadoop - nproc 5000000" | sudo tee --apend /etc/security/limits.conf
+echo "ulimit -i 515173" | tee --append ~/.bashrc
+echo "ulimit -u 515173" | tee --append ~/.bashrc
 echo "session required pam_limits.so" | sudo tee --append /etc/pam.d/common-session 
-
+echo "kernel.pid_max = 4194303" | sudo tee --append  /etc/sysctl.conf
+echo "kernel.threads-max = 4194303" | sudo tee --append  /etc/sysctl.conf
